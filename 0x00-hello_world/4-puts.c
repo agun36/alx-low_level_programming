e34c8488baecdd690b38cd74990c5d43463db432
Akindele Agun <aguntoroti10@gmail.com>
2023-02-16 05:12:05 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  * Return: return 0 means success
  */
 
-int main()
+int main(void)
 {
-	puts("\",Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
