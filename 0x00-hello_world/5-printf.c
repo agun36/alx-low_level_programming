246af7db23a040f007322a15a66825f21db06ac1
Akindele Agun <aguntoroti10@gmail.com>
2023-02-16 05:38:51 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio>
+
+/** 
+ * Main - Starting point
+ * Return : Always end with 0 (success)
+ */
+
+int main(void) 
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
