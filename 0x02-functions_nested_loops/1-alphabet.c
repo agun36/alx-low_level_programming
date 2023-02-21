b95447756a06a33e3cce7eaa5855504315b0f292
Akindele Agun <aguntoroti10@gmail.com>
2023-02-21 04:24:28 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int alphabets = 'a';
+
+	while (alphabets <= 'z)
+	{
+		_putchar(alphabets);
+		alphabets++;
+	}
+	_putchar('\n');
+}
