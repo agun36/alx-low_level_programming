3c2eab1f1c75e52a86e800d83459a6b44ebf647f
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-02-22 12:51:08 +0100

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-        i = 0;
+	i = 0;
 
 	/*
 	*  while (i < 10)
@@ -19,6 +19,6 @@ int main(void)
 	* putchar(i);
 	* }
 	*/
-        printf("Infinite loop avoided! \\o/\n");
-        return (0);
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
 }
