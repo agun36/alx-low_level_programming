4cd7920cee46a62d2a9492a805bff6ec9472d173
Akindele Agun <aguntoroti10@gmail.com>
2023-02-27 03:27:02 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_rev - prints a string, in reverse, followed by a new line.
+  * @s: stringto print
+  */
+
+void print_rev(char *s)
+{
+	int a, b;
+
+	b = 0;
+
+	while (s[n] != '\0')
+		b++;
+
+	a = b - 1;
+
+	while (a >= 0)
+	{
+		_putchar(s[a]);
+		a++;
+	}
+	_putchar('\n');
+}
