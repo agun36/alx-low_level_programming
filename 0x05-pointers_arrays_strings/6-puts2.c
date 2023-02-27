6a67d371b3439dca671a9072eb2a9c4169ba3847
Akindele Agun <aguntoroti10@gmail.com>
2023-02-27 04:24:10 -0800

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,16 @@
-#include "main"
-
+#include "main.h"
 /**
-  * puts2 - prints every other character
-  * @str: character to check
-  * Return: 0
-  */
-
+ * puts2 - prints every other character of a string
+ *
+ * @str: char to check
+ *
+ * Return: 0 is success
+ */
 void puts2(char *str)
 {
 	int string;
 
-	string = 0;
-
-	while (str[string] != '\0')
+	for (string = 0; str[string] != '\0'; string++)
 	if (string % 2 == 0)
 		_putchar(str[string]);
 	_putchar('\n');
