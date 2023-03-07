2473d2e2bfe88767312072d04033f2db34342558
Akindele Agun <aguntoroti10@gmail.com>
2023-03-07 04:28:25 -0800

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,11 +4,11 @@
  * @n:int
  * Return:int
  */
-int factorial(int n )
+int factorial(int n)
 {
 	if (n < 0)
-		return -1;
+		return (-1);
 	if (n == 0)
-		return 1;
-	return n*factorial(n - 1);
+		return (1);
+	return n * factorial(n - 1);
 }
