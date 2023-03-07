4fa181bbee7b3bfe6c044a0209b08c72965fb78e
Akindele Agun <aguntoroti10@gmail.com>
2023-03-07 03:54:28 -0800

    3-factorial.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - length of string
+ * @s:string
+ * Return:int
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _stren_recursion(s + 1);
+}
