2b47cc8ff88147d0dfd795ad90d8a4463f14e0d7
Akindele Agun <aguntoroti10@gmail.com>
2023-03-07 03:41:20 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * _puts_recursion - print a string
  * @s:string
  * Return:void
  */
-void _puts_recursion(char*s)
+
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
