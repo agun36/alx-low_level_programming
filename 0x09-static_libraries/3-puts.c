a89f21082ec8fb9637570f9a58b8ce479b5ef705
Akindele Agun <aguntoroti10@gmail.com>
2023-03-10 05:32:43 -0800

    0x09-static_libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _puts - prints a string, followed by a new line, to stdout.
+  * @str: the string
+  * Return: the lenght of the string
+  */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - check the code
+ *@n: check n
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
