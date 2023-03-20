d7647f0eff18b6b08f9b334f125e1c1ba86cda93
Akindele Agun <aguntoroti10@gmail.com>
2023-03-20 07:57:40 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,8 +10,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-	printf("Name:%s\n", (d->name == NULL) ? "(nil)" : d->name);
-	printf("Age:%f\n", d->age);
-	printf("Owner:%s\n", (d->owner == NULL) ? "(nil)" : d->owner);
+	printf("Name: %s\n", (d->name == NULL) ? "(nil)" : d->name);
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", (d->owner == NULL) ? "(nil)" : d->owner);
 	}
 }
