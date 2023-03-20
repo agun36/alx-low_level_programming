f85106d58a70d8591c1267bc3421de503131a043
Akindele Agun <aguntoroti10@gmail.com>
2023-03-20 07:16:22 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,16 +8,18 @@
  * @owner: Third member
  */
 
-struct dog 
+struct dog
 {
 	char *name;
 	float *age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 /*Define a new type dog_t as a new name for the type struct dog.*/
+
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
