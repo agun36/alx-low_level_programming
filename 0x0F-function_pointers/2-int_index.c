639e71a0943907ac7c3c5d518eeea7c39ba70b8e
Akindele Agun <aguntoroti10@gmail.com>
2023-03-22 08:57:44 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,15 +14,17 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
+
 	if (array == NULL || cmp == NULL)
 		return (-1);
 	if (size > 0)
-	int i;
 
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 			return (i);
 	}
+	
 	return (-1);
 }
