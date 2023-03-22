512acecf4109650bfc88c0b1c30f91490106ac94
Akindele Agun <aguntoroti10@gmail.com>
2023-03-22 08:14:39 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array == NULL || action == NULL)
+	if (array && action)
 	{
 		for (size_t i = 0; i < size; i++)
 		{
