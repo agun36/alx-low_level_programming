041ab4e7c6ef0d6da938c807fb92db03844c329c
agun36 <aguntoroti10@gmail.com>
2023-03-30 04:22:51 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+/*
+ * Apply the constructor attribute to startupfun()
+ * so that it is executed before main()
+*/
+	void startupfun(void) __attribute__ ((constructor));
+
+/*
+ * Apply the destructor attribute to cleanupfun()
+ *so that it is executed after main()
+ */
+
+	void cleanupfun(void)__attribute__ ((destructor));
+
+
 /**
  * print_before_main - Prints a message before the main function is executed.
  *
