b75cf0d1df3a72e512f09e99b26d36b2dc34b3e5
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-04-04 17:09:43 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
+	const listint_t *tmp = h;
 	size_t count = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		count++;
-		h = h->next;
-	}
+	if (h != NULL)
+		while (tmp != NULL)
+		{
+			printf("%d\n", tmp->n);
+			count++;
+			tmp = tmp->next;
+		}
 	return (count);
 }
