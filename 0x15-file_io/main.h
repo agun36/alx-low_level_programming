abb1f954dd71cdad0b43a607911f11e3937b8f5e
agun36 <aguntoroti10@gmail.com>
2023-04-07 14:55:38 -0700

    100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,6 +13,7 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <fcntl.h>
+#include <elf.h>
 
 /* All the function prototype*/
 void check_args(int argc);
@@ -20,6 +21,16 @@ void check_read(ssize_t check, char *file, int fd_from, int fd_to);
 void check_write(ssize_t check, char *file, int fd_from, int fd_to);
 void check_close(int check, int fd);
 void copy_file(const char *src_file_path, const char *target_file_path);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
