c3dbe2d43126c8ff3947c314fecd6564fe747232
agun36 <aguntoroti10@gmail.com>
2023-04-07 08:34:23 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,39 @@
 #include "main.h"
 
 /**
-  * create_file - creates a file 
+  * create_file - creates a fil
   * @filename: name of file to create
+  * @text_content: is a Null terminator string
   *
   * Return: 1 on success, -1 on failure
   */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t lenght = 0;
+	int result = -1;
+	int o = -1;
+	int w = -1;
+	int len = 0;
 
-	if (filename == NULL)
-		return (-1);
-
-	fd = open(filename, O_CREAT | O_WRONLY |O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
-
-	if (text_content != NULL)
+	if (filename != NULL)
 	{
-		while (text_content[lenght])
-			lenght++;
-		if (write(fd,text_content, lenght) != lenght)
-			return (-1);
+		if (text_content != NULL)
+		{
+			for (len = 0; text_content[len]; len++)
+				;
+		}
+
+		o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		if (o != -1)
+		{
+			w = write(o, text_content, len);
+
+			if (w != -1 && w == len)
+			{
+				result = 1;
+			}
+			close(o);
+		}
 	}
 
-	close(fd);
-
-	return(1);
+	return (result);
 }
