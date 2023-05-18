38210363c24815f9a0c6d060a40ebbee9ac4b872
agun36 <aguntoroti10@gmail.com>
2023-05-18 11:24:55 +0000

    0x17-doubly_linked_lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all elements of a doubly linked list.
+ * @h: Pointer to the head of the list.
+ * Return: Number of nodes.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	if (h == NULL)
+		return (count);
+	
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @n: Value to be assigned to the new node.
+ * Return: Address of the new element, or NULL if it failed.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *temp;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return NULL;
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+
+		temp->next = new_node;
+		new_node->prev = temp;
+	}
+
+	return new_node;
+}
