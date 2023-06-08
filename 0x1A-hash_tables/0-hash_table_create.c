27a956eb141c1b03143f8176cd8f8c84172d8305
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-08 20:00:18 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 	unsigned long int i;
 
+	if (size == 0)
+		return (NULL);
+
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
