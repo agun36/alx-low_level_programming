5a452e1c35cdb2cc501cac28fb650d786e8adc4e
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-08 19:10:14 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,6 +2,10 @@
 #include <string.h>
 #include "hash_tables.h"
 
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
 /**
  * create_node - Creates a new hash node
  * @key: The key string
@@ -12,14 +16,13 @@
 hash_node_t *create_node(const char *key, const char *value)
 {
 	hash_node_t *new_node = malloc(sizeof(hash_node_t));
-
+	
 	if (new_node == NULL)
 		return (NULL);
-
+	
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = NULL;
-
 	return (new_node);
 }
 
@@ -35,12 +38,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *new_node = NULL, *temp = NULL;
-
+	
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
-
+	
 	index = key_index((unsigned char *)key, ht->size);
-
+	
 	temp = ht->array[index];
 	while (temp != NULL)
 	{
@@ -54,29 +57,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		temp = temp->next;
 	}
-
-	new_node = malloc(sizeof(hash_node_t));
+	
+	new_node = create_node(key, value);
 	if (new_node == NULL)
 		return (0);
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-
+	
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
+	
 	return (1);
-
 }
