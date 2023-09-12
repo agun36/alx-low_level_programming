b4bb92f03604db17e2084dacc645d6d8635fc041
agun36 <aguntoroti10@gmail.com>
2023-09-12 15:39:22 +0100

    main

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,12 +12,12 @@
 int linear_search(int *array, size_t size, int value)
 {
 	if (array == NULL)
-		return -1;
+		return (-1);
 
 	for (size_t i = 0; i < size; i++)
 	{
 		if (array[i] == value)
-			return i;
+			return (i);
 	}
-	return -1;
+	return (-1);
 }
