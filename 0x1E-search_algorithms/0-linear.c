80a56a3675264dfe719de9f86b655a3d46b3cd15
agun36 <aguntoroti10@gmail.com>
2023-09-12 15:58:15 +0100

    main

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,17 +13,15 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (array == NULL)
+	if (array != NULL)
 	{
-		return (-1);
-	}
-
-	for (i = 0; i < size; i++)
-	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1);
