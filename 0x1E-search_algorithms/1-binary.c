ff829b476f8dfa2b8b3595f0091872d3ba86739d
agun36 <aguntoroti10@gmail.com>
2023-09-12 17:16:26 +0100

    main

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - entry point of the
+ * function
+ * @array: pointer to the first element
+ * of the array to search in
+ * @size: is the number of elements
+ * in array
+ * @value: is the value to search for
+ * Return:  your function must
+ * return -1 */
+int binary_search(int *array, size_t size, int value)
+{
+	int low = 0;
+	int high = size - 1;
+	int mid;
+	int i;
+	if (array != NULL)
+	{
+		while (low <= high)
+		{
+			printf("Searching in array: ");
+			for (i = 0; i < low; i++)
+				printf("%d, ", array[i]);
+			printf("%d\n", array[i]);
+			mid = (low + high) / 2;
+			if (array[mid] == value)
+				return (mid);
+			else if (array[mid] < value)
+				low = mid + 1;
+			else
+				high = mid - 1;
+		}
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -17,11 +17,12 @@
  */
 typedef struct listint_s
 {
-	int n;
-	size_t index;
-	struct listint_s *next;
+ int n;
+ size_t index;
+ struct listint_s *next;
 } listint_t;
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
